8e9ed153346994baec4da7bbe237133be486384e
AJackTi <AJackTi@AJACKTI>
2020-03-19 18:36:36 +0700

    7. Routing in Angular

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 
 import { AuthService } from "../_services/auth.service";
 import { AlertifyService } from "../_services/alertify.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-nav",
@@ -13,7 +14,8 @@ export class NavComponent implements OnInit {
 
   constructor(
     public authService: AuthService,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private router: Router
   ) {}
 
   ngOnInit() {}
@@ -27,6 +29,9 @@ export class NavComponent implements OnInit {
       error => {
         console.log(error);
         this.alertify.error(error);
+      },
+      () => {
+        this.router.navigate(["/members"]);
       }
     );
   }
@@ -43,5 +48,6 @@ export class NavComponent implements OnInit {
     localStorage.removeItem("token");
     console.log("logged out");
     this.alertify.message("logged out");
+    this.router.navigate(["/home"]);
   }
 }
